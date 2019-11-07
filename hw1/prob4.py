85d6c4381467119cb8c3c93b35e308591a97e4c9
r888800009 <r888800009@gmail.com>
2019-11-07 19:43:00 +0800

    Update prob4.py

--- hw1/prob4.py
+++ hw1/prob4.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 copyright = 'Copyright@Lin Yong Xiang 10612150'
-def plotFigure(f, title):
-    x = np.linspace(-np.pi, np.pi, 100)
+def plotFigure(f, title, istart, iend):
+    x = np.linspace(istart, iend, 100)
     y = f(x)
     plt.plot (x, y)
     plt.xlabel('x')
@@ -20,11 +20,11 @@ def fc(x): return -x * np.cos(x) + x
 def fd(x): return np.cos(x) * np.sin(x) + np.cos(x)
 def fe(x): return np.tan(x) - x - 1
 
-plotFigure(fa, 'IVP A solve: (x - 1) / x')
-plotFigure(fb, 'IVP A solve: sin((x ** 2) / 2)')
-plotFigure(fc, 'IVP A solve: -x * cos(x) + x')
-plotFigure(fd, 'IVP A solve: cos(x) * sin(x) + cos(x)')
-plotFigure(fe, 'IVP A solve: tan(x) - x - 1')
+plotFigure(fa, 'IVP A solve: (x - 1) / x', 0, 1)
+plotFigure(fb, 'IVP A solve: sin((x ** 2) / 2)', 0, 2 * np.pi)
+plotFigure(fc, 'IVP A solve: -x * cos(x) + x', 0, 8 * np.pi)
+plotFigure(fd, 'IVP A solve: cos(x) * sin(x) + cos(x)', 0, 4 * np.pi)
+plotFigure(fe, 'IVP A solve: tan(x) - x - 1', 0, 4 * np.pi)
 
 print('checks')
 x = symbols('x')
